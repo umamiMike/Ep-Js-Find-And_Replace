8d82f2444bc1c6d2898fb300ff84846c31a872af
umamiMike <michael.w.chastain@gmail.com>
2015-09-11 15:20:15 -0700

    add more commented out experimental functions and final functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var findReplace = function(w_find,w_replace)
-    { w_find =  w_find + "\\b";
-    console.log(w_find);
+    { w_find =  "\\b" + w_find + "\\b";
         var re =  new RegExp (w_find,"gi");
         var str = $("#text_to_work_on").text();
         var newstr = str.replace(re, w_replace);
@@ -23,17 +22,20 @@ $(document).ready(function() {
 //was tricky to implement with keyup due to not caching of original text and
 //replacing that in subsequent alterations
 
-// $(document).on("keyup", ".main__form", function () {
-//         console.log("keyup occured");
-//         var find_word = $('#word_find').val();
-//         var replace_word = $('#word_replace').val();
+// $('#word_replace').keyup(function(){
+//
+//     var find_word = $('#word_find').val();
+//     var replace_word = $('#word_replace').val();
 //     var replaced = findReplace(find_word,replace_word);
-//     console.log(replaced);
+//
 //     $("#text_to_work_on").text(replaced);
+//
+//     $('#word_find').val(replace_word);
+//
+//         console.log($('#word_find').val());
+//
+// })
 
-    //event.preventDefault();
-
-//});
 
 // var scrapeSite = function (theURL) {
 //
